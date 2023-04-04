0bd74070943e64aa01f2f3fb616e64921ba388eb
Julien Taquet <julientaq@mac16.local>
2023-04-04 12:41:35 +0200

    myOwnFirstAstroNvim

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,26 +1,45 @@
 -- customize mason plugins
 return {
-  -- use mason-lspconfig to configure LSP installations
-  {
-    "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
-    opts = {
-      -- ensure_installed = { "lua_ls" },
+    {
+        "williamboman/mason.nvim",
+        cmd = {
+            "Mason", "MasonInstall", "MasonUninstall", "MasonUninstallAll",
+            "MasonLog", "MasonUpdate", -- AstroNvim extension here as well
+            "MasonUpdateAll" -- AstroNvim specific
+        },
+        opts = {
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_uninstalled = "✗",
+                    package_pending = "⟳"
+                }
+            },
+            keymaps = {
+                -- Keymap to apply language filter
+                apply_language_filter = "F"
+            }
+        },
+        config = require "plugins.configs.mason"
+    }, {
+        "williamboman/mason-lspconfig.nvim",
+        -- overrides `require("mason-lspconfig").setup(...)`
+        opts = {
+            -- ensure_installed = { "lua_ls" },
+        }
     },
-  },
-  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-  {
-    "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
-    opts = {
-      -- ensure_installed = { "prettier", "stylua" },
-    },
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = {
-      -- ensure_installed = { "python" },
-    },
-  },
+    -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+    {
+        "jay-babu/mason-null-ls.nvim",
+        -- overrides `require("mason-null-ls").setup(...)`
+        opts = {
+            -- ensure_installed = { "prettier", "stylua" },
+        }
+    }, {
+        "jay-babu/mason-nvim-dap.nvim",
+        -- overrides `require("mason-nvim-dap").setup(...)`
+        opts = {
+            -- ensure_installed = { "python" },
+        }
+    }
 }
---
+++ plugins/minisurround.lua
@@ -0,0 +1,8 @@
+return {
+
+    'echasnovski/mini.surround',
+    version = false,
+    lazy = false,
+    config = function() require('mini.surround').setup() end
+
+}
